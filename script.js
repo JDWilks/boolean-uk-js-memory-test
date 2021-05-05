2ac3267f7c2ffe4497c4e36b89383f557a6d4479
Jonathon Wilks <jonathondwilks@icloud.com>
2021-05-05 09:13:07 +0100

    where i'm at with the memory code

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-// Description
-// Test the user's memory with this simple little game. This will also put your skills up to the test!
+// code that sort of works - enter in this function
 
-// Instructions
+function askForGuesses(howmanyGuesses) {
+ 
+    let correctNumberOfGuesses = 0
+
+    for (i =0; i<4; i++){
+    
+        let answer = Number(prompt("Please enter your guess"))
+    
+        if (answer === arrayOfNumbers[0] || answer === arrayOfNumbers[1] || answer === arrayOfNumbers[2] || answer === arrayOfNumbers[3]){
+        alert("you are correct")
+        } else {
+            alert("You didn't guess correct")
+        }
+    }
+}
+
+// then run this function
+
+function getRandomNumber(max) {
+    
+    arrayOfNumbers = []
+
+    for(i = 0; i < 4; i++){
+
+    let randomNumber = Math.floor(Math.random() * max)
+    
+    arrayOfNumbers.push(randomNumber)
+}
+
+alert("Remember these 4 numbers please. \nClick OK when you are ready to play. \n" + arrayOfNumbers)
+arrayOfGuessedNumbers = []
+setTimeout(askForGuesses(4), 3000);
+}
+
+
+// the below is my brain trying to figure it out step by step...
+
+
+
 // - Show the user 4 random numbers between 1 and 100
 
 // generate 4 random numbers between 1 and 100
@@ -37,9 +74,7 @@ alert("Remember these 4 numbers please. \nClick OK when you are ready to play. \
 }
 
 
-// - After 15 seconds, ask them to enter each number in order ✅
-
-
+// - After 15 seconds, ask them to enter each number in order ✅ although Rico says ❌ due to putting in 4 numbers in the same prompt
 
 function getRandomNumber(max) {
     
@@ -58,45 +93,8 @@ setTimeout(function(){prompt("Enter the 4 numbers you previously saw on the scre
 }
 
 // - Let them know how many they got right!
-// - i need to store the numbers entered in the prompt to an array to compare
-
-
-// - i need to compare the arrayOfNumbers with the 
-
-
-// Challenge 1
-// When asking the user for the numbers, add ordinality to each number you ask. i.e.:
-// - Enter the 1st number
-// - Enter the 2nd number
-// Etc...
-
-// Challenge 2
-// Turn your whole program into a function that will allow you to change the number of numbers 
-// displayed and asked for by just entering the desired number as an argument when calling the function
-
-
-
-
-
-//// messing around with code that works
-
-function getRandomNumber(max) {
-    
-    arrayOfNumbers = []
-
-    for(i = 0; i < 4; i++){
-
-    let randomNumber = Math.floor(Math.random() * max)
-    
-    arrayOfNumbers.push(randomNumber)
-}
-
-alert("Remember these 4 numbers please. \nClick OK when you are ready to play. \n" + arrayOfNumbers)
-setTimeout(function(){ let myNumbers = prompt("Enter the 4 numbers you previously saw on the screen..." ); }, 5000);
-
-}
-
-
+// - i need to store the numbers entered in the prompt to an array to compare - Rico says no ❌
+// - the guessed numbers are currently stored in a array as a string ❌
 
 function getRandomNumber(max) {
     
@@ -114,3 +112,62 @@ arrayOfGuessedNumbers = []
 setTimeout(function(){let myNumbers = prompt("Enter the 4 numbers you previously saw on the screen..." ); arrayOfGuessedNumbers.push(myNumbers)}, 5000);
 
 }
+
+//// messing around with above code that works that maybe wont after i've added stuff
+
+// the below code takes in the fuction i've written called askedForGuesses although the settimeout doesn't work
+
+function getRandomNumber(max) {
+    
+    arrayOfNumbers = []
+
+    for(i = 0; i < 4; i++){
+
+    let randomNumber = Math.floor(Math.random() * max)
+    
+    arrayOfNumbers.push(randomNumber)
+}
+
+alert("Remember these 4 numbers please. \nClick OK when you are ready to play. \n" + arrayOfNumbers)
+arrayOfGuessedNumbers = []
+setTimeout(askForGuesses(4), 3000);
+}
+
+//// rico said to use 4 individual prompts for the user guesses
+
+
+function askForGuesses(howmanyGuesses) {
+ 
+    let correctNumberOfGuesses = 0
+
+    for (i =0; i<4; i++){
+    
+        let answer = Number(prompt("Please enter your guess"))
+    
+        if (answer === arrayOfNumbers[0] || answer === arrayOfNumbers[1] || answer === arrayOfNumbers[2] || answer === arrayOfNumbers[3]){
+        alert("you are correct")
+        } else {
+            alert("You didn't guess correct")
+        }
+    }
+}
+
+
+  // Step 3: Count the correctNumberOfGuesses
+// i'm not sure how to do this
+
+
+
+
+
+
+// Challenge 1
+// When asking the user for the numbers, add ordinality to each number you ask. i.e.:
+// - Enter the 1st number
+// - Enter the 2nd number
+// Etc...
+
+// Challenge 2
+// Turn your whole program into a function that will allow you to change the number of numbers 
+// displayed and asked for by just entering the desired number as an argument when calling the function
+
